5125676f9b10655bc1782d92513f04323b76b26e
Monty Montgomery <monty@xiph.org>
2015-09-25 15:08:56 -0400

    Correct bounds overrun in motion compensation (1fmv8)
    1fmv8 does subpel resampling using a 6-tap filter that will extend
    outside the bounds of the old OD_UMV_PADDING around the reference
    buffers.  It's necessary to either increase the padding to take the
    extra filter width into account, or special-case the resampling filter
    in 1fmv8 the same way as it was special-cased in upsample8.
    
    To avoid the extra code complexity, we opt for increasing the buffer
    padding for now.  Before release, we should change to special-casing
    (ala the "emulated edge MC" approach of libav/FFmpeg).
    
    This patch also separates the conflated meanings of the padding and
    UMV clamp; they are now separate constants.
    
    Conflicts:
    	src/decode.c
    	src/encode.c

--- src/decode.c
+++ src/decode.c
@@ -59,7 +59,6 @@ static int od_dec_init(od_dec_ctx *dec, const daala_info *info,
   int ret;
   int pli;
   int imgi;
-  int i;
   (void)setup;
   dec->state.codec_mode = 1;
   ret = od_state_init(&dec->state, info);
@@ -71,8 +70,8 @@ static int od_dec_init(od_dec_ctx *dec, const daala_info *info,
   dec->user_mc_img = NULL;
   data_sz = 0;
   /*TODO: Check for overflow before allocating.*/
-  frame_buf_width = dec->state.frame_width + (OD_UMV_PADDING << 1);
-  frame_buf_height = dec->state.frame_height + (OD_UMV_PADDING << 1);
+  frame_buf_width = dec->state.frame_width + (OD_BUFFER_PADDING << 1);
+  frame_buf_height = dec->state.frame_height + (OD_BUFFER_PADDING << 1);
   for (pli = 0; pli < info->nplanes; pli++) {
     plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
     plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
@@ -94,8 +93,8 @@ static int od_dec_init(od_dec_ctx *dec, const daala_info *info,
       plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
       iplane = img->planes + pli;
       iplane->data = output_img_data
-        + (OD_UMV_PADDING >> info->plane_info[pli].xdec)
-        + plane_buf_width*(OD_UMV_PADDING >> info->plane_info[pli].ydec);
+        + (OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
+        + plane_buf_width*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
       output_img_data += plane_buf_width*plane_buf_height*n_out;
       iplane->xdec = info->plane_info[pli].xdec;
       iplane->ydec = info->plane_info[pli].ydec;
@@ -198,15 +197,15 @@ int daala_decode_ctl(daala_dec_ctx *dec, int req, void *buf, size_t buf_sz) {
 static void od_dec_blank_img(od_img *img) {
   int pli;
   int frame_buf_height;
-  frame_buf_height = img->height + (OD_UMV_PADDING << 1);
+  frame_buf_height = img->height + (OD_BUFFER_PADDING << 1);
   for (pli = 0; pli < img->nplanes; pli++) {
     int plane_buf_size;
     int plane_buf_offset;
     plane_buf_size =
      (frame_buf_height >> img->planes[pli].ydec) * img->planes[pli].ystride;
     plane_buf_offset =
-      (OD_UMV_PADDING >> img->planes[pli].ydec) * img->planes[pli].ystride
-      + (OD_UMV_PADDING >> img->planes[pli].xdec);
+      (OD_BUFFER_PADDING >> img->planes[pli].ydec) * img->planes[pli].ystride
+      + (OD_BUFFER_PADDING >> img->planes[pli].xdec);
     memset(img->planes[pli].data - plane_buf_offset, 128, plane_buf_size);
   }
 }
